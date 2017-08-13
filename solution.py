597588f50804c5d8f06a39b814eebb27c321b829
Gerald Brown <gemabrow@ucsc.edu>
2017-08-12 22:17:15 -0700

    naked twins functionality added. documentation ongoing

--- solution.py
+++ solution.py
@@ -1,6 +1,5 @@
 from heapq import heappush, heappop
 
-
 assignments = []
 
 
@@ -17,10 +16,10 @@ ROW_UNITS = [cross(r, COLS) for r in ROWS]
 COLUMN_UNITS = [cross(ROWS, c) for c in COLS]
 SQUARE_UNITS = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI')
                 for cs in ('123', '456', '789')]
-DIAG_UNITS = [parallel_concat(ROWS, COLS),
-              parallel_concat(reversed(ROWS), COLS)]
+DIAGONAL_UNITS = [parallel_concat(ROWS, COLS),
+                  parallel_concat(reversed(ROWS), COLS)]
 
-UNITLIST = ROW_UNITS + COLUMN_UNITS + SQUARE_UNITS + DIAG_UNITS
+UNITLIST = ROW_UNITS + COLUMN_UNITS + SQUARE_UNITS + DIAGONAL_UNITS
 UNITS = dict((s, [u for u in UNITLIST if s in u]) for s in BOXES)
 PEERS = dict((s, set(sum(UNITS[s], []))-set([s])) for s in BOXES)
 
@@ -41,8 +40,25 @@ def assign_value(values, box, value):
     return values
 
 
+def remove_from_peers(box, values):
+    """Remove values of a box from peers' choices.
+
+    Input: String referring to a box, Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after removal.
+    """
+    for peer in PEERS[box]:
+        for digit in values[box]:
+            if digit in values[peer]:
+                # TO-DO: Use regex replace here, instead
+                updated_peer_value = values[peer].replace(digit, "")
+                values = assign_value(values, peer, updated_peer_value)
+    return values
+
+
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
+    Precondition:
+        for each key in values, item strings are in ascending order of digits.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
 
@@ -51,9 +67,55 @@ def naked_twins(values):
     """
 
     # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
-    pass
+    #for unit in UNITLIST:
+    #    for digit in DIGITS:
+    #        # create dict with keys being all digits > current digit
+    #        relatives = {str(other_digit): []
+    #                     for other_digit in range(int(digit)+1, 10)}
+    #        for box in unit:
+    #            if digit in values[box] and len(values[box]) == 2:
+    #                # get secondary digit from potential twin
+    #                #print(values[box], type(values[box][-1]))
+    #                other_digit = values[box][-1]
+    #                #print(box)
+    #                # add box to list of possible naked twins
+    #                if other_digit is not digit:
+    #                    relatives[other_digit].append(box)
+    #
+    #        for other_digit, boxes in relatives.items():
+    #            # naked twin found
+    #            if len(boxes) == 2:
+    #                primary_box = boxes[0] 
+    #                secondary_box = boxes[1] 
+    #                # remove naked twin values from peers of primary box
+    #                for box in boxes:
+    #                    print(box, ": ", values[box])
+    #                values = remove_from_peers(primary_box, values)
+    #                #print(primary_box, values[primary_box]) 
+    #                # reassign naked twin values to secondary box
+    #                values = assign_value(values, secondary_box, values[primary_box])
+    #                #print(secondary_box, values[secondary_box])
+    #return values
+    viable_twins = [box for box in values.keys() if len(values[box]) == 2]
+    actual_twins = [(primary_box, secondary_box) for primary_box in viable_twins
+                    for secondary_box in PEERS[primary_box]
+                    if set(values[primary_box]) == set(values[secondary_box])]
 
+    for pair in actual_twins:
+        primary_box = pair[0]
+        secondary_box = pair[1]
+        twin_vals = set(values[primary_box])
+        primary_peers = set(PEERS[primary_box])
+        secondary_peers = set(PEERS[secondary_box])
+        joint_peers = primary_peers & secondary_peers
+
+        for peer in joint_peers:
+            if len(values[peer]) > 2:
+                updated_peer_value = ''.join(c for c in values[peer]
+                                             if c not in twin_vals)
+                values = assign_value(values, peer, updated_peer_value)
+
+    return values
 
 def grid_values(grid):
     """
@@ -72,14 +134,11 @@ def grid_values(grid):
     assert len(grid) == 81, "Sudoku grid is an invalid length"
 
     # process string into dict
-    values = {box: grid[index] if grid[index] in DIGITS else
-              DIGITS for index, box in enumerate(BOXES)}
+    values = {box: grid[index] if grid[index] in DIGITS else DIGITS
+              for index, box in enumerate(BOXES)}
 
-    # for index, box in enumerate(boxes):
-    #     updated_value = grid[index] if grid[index] in valid_digits \
-    #             else valid_digits
-    #     values = assign_value(values, box, updated_value)
-
+    for box, value in values.items():
+        values = assign_value(values, box, value)
     return values
 
 
@@ -113,20 +172,7 @@ def eliminate(values):
     for box, value in values.items():
         if len(value) == 1:
             values = assign_value(values, box, value)
-            values = remove_peer_choice(box, values)
-    return values
-
-
-def remove_peer_choice(box, values):
-    """Remove values of a box from peers' choices.
-
-    Input: String referring to a box, Sudoku in dictionary form.
-    Output: Resulting Sudoku in dictionary form after removal.
-    """
-    for peer in PEERS[box]:
-        if values[box] in values[peer]:
-            updated_peer_value = values[peer].replace(values[box], "")
-            values = assign_value(values, peer, updated_peer_value)
+            values = remove_from_peers(box, values)
     return values
 
 
@@ -152,18 +198,20 @@ def reduce_puzzle(values):
     stalled = False
     while not stalled:
         # Check how many boxes have a determined value
-        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        solved_values_before = len([box for box, value in values.items() if len(value) == 1])
 
         # Your code here: Use the Eliminate Strategy
         values = eliminate(values)
         # Your code here: Use the Only Choice Strategy
         values = only_choice(values)
-
+        # Naked Twins strategy
+        values = naked_twins(values)
+        
         # Check how many boxes have a determined value, to compare
-        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        solved_values_after = len([box for box, value in values.items() if len(value) == 1])
         # If no new values were added, stop the loop.
         stalled = solved_values_before == solved_values_after
-        # Sanity check, return False if there is a box with zero available values:
+        # Return False if there is a box with zero available values:
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
     return values
@@ -205,10 +253,12 @@ def solve(grid):
     Find the solution to a Sudoku grid.
     Args:
         grid(string): a string representing a sudoku grid.
-            Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     Returns:
-        The dictionary representation of the final sudoku grid. False if no solution exists.
+        The dictionary representation of the final sudoku grid.
+        False if no solution exists.
     """
+    values = grid_values(grid)
+    return search(values)
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
@@ -221,4 +271,5 @@ if __name__ == '__main__':
     except SystemExit:
         pass
     except:
-        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+        print('We could not visualize your board due to a pygame issue.\
+               Not a problem! It is not a requirement.')
